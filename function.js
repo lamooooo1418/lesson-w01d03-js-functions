db65e6ce7b6f7cd3224aec9b54bdb3c0dbf59703
Lamooooo1418 <lamooooo1418@gmail.com>
2020-01-21 17:11:28 +0300

    editing

---
+++ function.js
@@ -0,0 +1,35 @@
+const youRock = function(name) {
+
+
+    console.log('yourock' + name);
+
+
+};
+const square = function(num) {
+
+
+
+    total = num * num;
+    return total;
+};
+
+
+
+
+const cube = function(num1) {
+
+
+
+    total1 = num1 * num1 * num1;
+    return total1;
+};
+
+
+
+const toThefourth = function(num2) {
+
+
+
+    total2 = num2 * num2 * num2 * num2;
+    return total2;
+};
